1c8f248d7aaddd16431f093da4c15c9fec89e249
a.boboshko <a.boboshko@cognitivepilot.com>
2024-12-24 10:10:39 +0300

    in working

--- cmd/main/app.go
+++ cmd/main/app.go
@@ -1,7 +1,13 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"homevideo_for_xlsx_parser/pkg/logging"
+)
 
 func main() {
 	fmt.Println("Hello, World!")
+
+	var logger = logging.GetLogger()
+	logger.Info("Hello, World!")
 }
--- pkg/logging/logging.go
+++ pkg/logging/logging.go
@@ -2,19 +2,41 @@ package logging
 
 import (
 	"fmt"
+	"os"
 	"path"
 	"runtime"
 
 	"github.com/sirupsen/logrus"
 )
 
-func Init() {
+var e *logrus.Entry
+
+type Logger struct {
+	*logrus.Entry
+}
+
+func GetLogger() Logger {
+	return Logger{e}
+}
+
+func init() {
 	var log = logrus.New()
 	log.SetReportCaller(true)
-	log.Formatter = &logrus.JSONFormatter{
+	log.Formatter = &logrus.TextFormatter{
 		CallerPrettyfier: func(frame *runtime.Frame) (function string, file string) {
 			filename := path.Base(frame.File)
 			return fmt.Sprintf("%s()", frame.Function), fmt.Sprintf("%s:%d", filename, frame.Line)
 		},
+		FullTimestamp: true,
 	}
+
+	log_file, err := os.OpenFile("../../logs/log.txt", os.O_CREATE | os.O_WRONLY | os.O_APPEND, 0666)
+	if err != nil {
+		panic(err)
+	}
+
+	log.SetOutput(log_file)
+	log.SetLevel(logrus.TraceLevel)
+
+	e = logrus.NewEntry(log)
 }
